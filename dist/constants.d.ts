5d11cb21f9f6b8a9e7ff482c3c9a978764fb3213
kkkkk-npc <kkkkknpc@proton.me>
2024-09-11 18:58:18 +0800

    feat:update contract

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,10 +1,10 @@
-export declare const FACTORY_ADDRESS = "0xD7958C86786bfeEe1350F764031109901582d939";
+export declare const FACTORY_ADDRESS = "0x434f93bc85e709C7c6760f1DfE793D35501EA671";
 export declare const FACTORY_ADDRESS_MAP: {
     85432: string;
     1: string;
 };
 export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export declare const POOL_INIT_CODE_HASH = "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
+export declare const POOL_INIT_CODE_HASH = "0x1e85f501b45deb56047d2d981e42784125caf47d6c30afeaab8a2a9fc0498037";
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
  */
